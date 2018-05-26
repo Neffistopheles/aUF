d442e1d68199737a310ad67d82a18609445869a3
Anthony Eadicicco <neffi@neffi.net>
2018-05-25 20:56:21 -0400

    .

--- handlers.lua
+++ handlers.lua
@@ -8,32 +8,32 @@ aUF.h = {}
 
 
 -- Bar handlers
-aUF.bar = {}
+aUF.h.bar = {}
 
-function aUF.bar.value(self, current, total)
+function aUF.h.bar.value(self, current, total)
   if total == 0 then current = 0 end
   self:SetMinMaxValues(current, total)
   self:SetValue(current)
 end
 
-function aUF.bar.rgba(self, r, g, b, a)
+function aUF.h.bar.rgba(self, r, g, b, a)
   self:SetVertexColor(r, g, b, a or 1)
 end
 
-function aUF.bar.rgb(self, r, g, b)  -- drop alpha
+function aUF.h.bar.rgb(self, r, g, b)  -- drop alpha
   self:SetVertexColor(r, g, b)
 end
 
-function aUF.bar.classcolor(self, class)
+function aUF.h.bar.classcolor(self, class)
   local c = (CUSTOM_CLASS_COLORS or RAID_CLASS_COLORS)[class or 'WARRIOR']
   self:SetVertexColor(c.r, c.g, c.b)
 end
 
 
 -- Text handlers
-aUF.text = {}
+aUF.h.text = {}
 
-function aUF.text.absshort(self, current, total)
+function aUF.h.text.absshort(self, current, total)
   if total == 0 then current = 0 end
   local suf, fmt, val
   if current > 10 ^ 6 then
@@ -56,25 +56,25 @@ function aUF.text.absshort(self, current, total)
   self:SetText(string.format(fmt, val) .. suf)
 end
 
-function aUF.text.perc(self, current, total)
+function aUF.h.text.perc(self, current, total)
   if total == 0 then current = 0 end
   self:SetText(string.format('%d', current / total))
 end
 
-function aUF.text.perc1(self, current, total)
+function aUF.h.text.perc1(self, current, total)
   if total == 0 then current = 0 end
   self:SetText(string.format('%.1f', current / total))
 end
 
-function aUF.text.rgba(self, r, g, b, a)
+function aUF.h.text.rgba(self, r, g, b, a)
   self:SetTextColor(r, g, b, a or 1)
 end
 
-function aUF.text.rgb(self, r, g, b)
+function aUF.h.text.rgb(self, r, g, b)
   self:SetTextColor(r, g, b)
 end
 
-function aUF.text.classcolor(self, class)
+function aUF.h.text.classcolor(self, class)
   local c = (CUSTOM_CLASS_COLORS or RAID_CLASS_COLORS)[class or 'WARRIOR']
   self:SetTextColor(c.r, c.g, c.b)
 end
--- main.lua
+++ main.lua
@@ -8,43 +8,156 @@ _G['aUF'] = aUF
 
 
 -- UFs
-function aUF:spawnuf(unit)
+aUF.units = setmetatable({}, {
+  __call = function(units) return next, units end
+})
+
+do
+  local function uf_OnClick(button)
+    local self = this  -- magic
+    if button == 'LeftButton' then
+      if SpellIsTargeting() then
+        SpellTargetUnit(self.unit)
+      elseif CursorHasItem() then
+        if self.unit == 'player' then
+          AutoEquipCursorItem()
+        else
+          DropItemOnUnit(self.unit)
+        end
+      else
+        TargetUnit(self.unit)
+      end
+    elseif button == 'RightButton' then
+      if SpellIsTargeting() then
+        StopSpellTargeting()
+      else
+        -- show menu
+      end
+    end
+  end
   
+  function aUF:uf(unit)
+    local frame = self.units[unit]
+    if frame then return frame end
+    
+    frame = CreateFrame('Button', 'aUF_' .. unit, UIParent)
+    frame:EnableMouse(true)
+    frame:RegisterForClicks('AnyDown')
+    frame:SetScript('OnClick', uf_OnClick)
+    frame:SetScript('OnEnter', UnitFrame_OnEnter)  -- FrameXML/UnitFrame.lua#47
+    frame:SetScript('OnLeave', UnitFrame_OnLeave)  -- FrameXML/UnitFrame.lua#79
+    frame.unit = unit
+    self.units[unit] = frame
+    
+    if self:iswhackyunit(unit) then self:pollforwhackyunit(unit) end
+    
+    return frame
+  end
 end
 
 
 -- Tag updaes
 function aUF:updatetag(tag, unit, a1, a2, a3, a4, a5, a6, a7, a8, a9, a10)
-  for frame in self:frames(unit) do
+  for unit, frame in self:units() do
     -- throw to all :connect()'d functions
   end
 end
 
 
--- Unit resolution
--- Functions to be called when a unit has been potentially created, changed, or
--- destroyed.
+-- Unit logic
+aUF.WHACKY_HZ = 10
+
 do
-  local rreg = {}
+  local holistic_units = { player = true, target = true, pet = true }
   
-  function aUF:onunitresolved(callback)
-    table.insert(rreg, callback)
+  for i = 1,(MAX_PARTY_MEMBERS or 4) do
+    holistic_units[string.format('party%d', i)] = true
+    holistic_units[string.format('party%dpet', i)] = true
   end
   
-  local function resolved(unit)
-    for i = 1, #rreg do
-      local succ, err = pcall(rreg[i], unit)
-      if not succ then aUF.softerror(err) end
-    end
+  for i = 1,(MAX_RAID_MEMBERS or 40) do
+    holistic_units[string.format('raid%d', i)] = true
+    holistic_units[string.format('raid%dpet', i)] = true
   end
   
-  uLib:eventreg('PLAYER_TARGET_CHANGED', function() resolved('target') end)
-  uLib:eventreg('UPDATE_MOUSEOVER_UNIT', function() resolved('mouseover') end)
-  uLib:eventreg('PLAYER_ENTERING_WORLD', function()
-    for unit in aUF:units() do
-      resolved(unit)
-    end
-  end)
-  uLib:eventreg('UNIT_PET', function(unit) resolved(unit .. 'pet') end)
+  function aUF:iswhackyunit(unit)
+    return not holistic_units[unit]
+  end
 end
 
+local resolution_callbacks = {}
+
+function aUF:onunitresolved(callback)
+  self.assertat(2, type(callback) == 'function', 'bad argument #1')
+  table.insert(resolution_callbacks, callback)
+end
+
+local function resolved(unit)
+  for i = 1, #resolution_callbacks do
+    local succ, err = pcall(resolution_callbacks[i], unit)
+    if not succ then aUF.softerror(err) end
+  end
+  -- if `unit` has a set in derivative_unit, then scan that set and set up
+  -- (exists) or destroy (doesn't exist) polling.
+end
+
+aUF:eventreg('PLAYER_TARGET_CHANGED', function() resolved('target') end)
+aUF:eventreg('PLAYER_ENTERING_WORLD', function()
+  for unit in aUF:units() do
+    resolved(unit)
+  end
+end)
+aUF:eventreg('UNIT_PET', function(unit) resolved(unit .. 'pet') end)
+aUF:eventreg('RAID_ROSTER_UPDATE', function()
+  for i = 1, (MAX_RAID_MEMBERS or 40) do resolved('raid' .. i) end
+end)
+aUF:eventreg('PARTY_MEMBERS_CHANGED', function()
+  for i = 1, (MAX_PARTY_MEMBERS or 4) do resolved('party' .. i) end
+end)
+
+aUF:eventreg('UPDATE_MOUSEOVER_UNIT', function()
+  -- Mouseover only receives this event, so it's actually whacky
+  resolved('mouseover')
+end)
+
+aUF:onunitresolved(function(unit)
+  local frame = aUF.units[unit]
+  if not frame then return end
+  if UnitExists(unit) then
+    frame:Show()
+  else
+    frame:Hide()
+  end
+end)
+
+do
+  local whacky_units = {}
+  local whacky_status = {}
+  local whacky_callbacks = {}
+  
+  function aUF:pollforwhackyunit(unit)
+    whacky_units[unit] = true
+  end
+  
+  function aUF:onwhackypoll(callback)
+    self.assertat(2, type(callback) == 'function', 'bad argument #1')
+    table.insert(whacky_callbacks, callback)
+  end
+  
+  aUF:timerreg(aUF.WHACKY_HZ, true, function()
+    for unit in pairs(whacky_units) do
+      local oldstatus = whacky_status[unit]
+      local exists = UnitExists(unit)
+      if exists ~= oldstatus then
+        whacky_status[unit] = exists
+        resolved(unit)
+      end
+      if exists then
+        for i = 1,#whacky_callbacks do
+          local succ, err = pcall(whacky_callbacks[i], unit)
+          if not succ then aUF.softerror(err) end
+        end
+      end
+    end
+  end)
+end
